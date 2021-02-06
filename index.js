9f11c59d09eca582c56043d53c8dc8f7d90a3d20
FFooXXxx <78666139+FFooXXxx@users.noreply.github.com>
2021-02-07 02:02:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ bot.on('ready', () => {
 app.get('/:name/:text', async (req, res) => {
     let name = req.params.name;
     let RawText = req.params.text;
-    let text = RawText.replace('_', ' ').toString('utf8');
+    let text = RawText.replace('_', ' ').toString('ascii');
     
     let embed = new Discord.MessageEmbed()
         .setTitle(`GMOd Message`)
