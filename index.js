05b7d7a4755b8e35ecb157d617fc1984c82958ed
OreoFox <nexagonice@gmail.com>
2021-02-07 01:33:15 +0300

    1

---
+++ index.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const express = require('express');
+const app = express();
+let token = 'NzMxOTAyNjEzODA4NzQyNDEy.Xwszzw.-AuqjM3BzDq57a_w49i_iSisYes';
+let prefix = '*';
+
+bot.on('ready', () => {
+    console.log(`Let's make some kung-fu with ${bot.user.username}`);
+    bot.generateInvite(["ADMINISTRATOR"]).then(link => {
+        console.log(link);
+    })
+
+    bot.user.setActivity("WILYFOX");
+});
+
+app.get('/:name/:text', async (req, res) => {
+    let name = req.params.name;
+    let text = req.params.text;
+
+    let embed = new Discord.MessageEmbed()
+        .setTitle(`GMOd Message`)
+        .setColor(`#dcdcdc`)
+        .setDescription(`All Users Viewer | ${increment}`)
+        .addFields(
+            { name: 'Username', value: name },
+            { name: 'Text', value: text },
+        )
+
+    await bot.channels.fetch('781598931409829899').then(channel => {
+        channel.send(embed);
+    });
+    
+    console.log(name, text);
+});
+
+app.listen(3000, () => {
+    bot.login(token);
+})
