01816c51381473872932c716aa1e85b136457052
FFooXXxx <78666139+FFooXXxx@users.noreply.github.com>
2021-02-09 11:49:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -52,6 +52,7 @@ app.post('/', urlencodedParser, async (req, res) => {
         .addFields(
             { name: 'Тип сообщения', value: type },
             { name: 'Содержание', value: '```' + text.trim() + '```' },
+            { name: 'URL', value: avatarurl },
         )
         .setAuthor(avatarurl)
 
